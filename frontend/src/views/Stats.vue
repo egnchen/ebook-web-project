974e896049ef4236e9bf7e1b51bec904300a6a40
eyekill <1229522864@qq.com>
2019-05-27 18:41:27 +0800

    Interactive stats

--- frontend/src/views/Stats.vue
+++ frontend/src/views/Stats.vue
@@ -15,6 +15,66 @@
             <v-card-text>
                 <v-chart :options="bookStat" />
             </v-card-text>
+            <v-card-actions>
+                <v-spacer />
+                <span v-if="!!startDate && !!endDate">从{{startDate}}到{{endDate}}</span>
+                <v-dialog v-model="datePickerDialog" width="500">
+                    <template v-slot:activator="{on}">
+                         <v-btn color="primary" v-on="on">修改日期</v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-title>修改日期</v-card-title>
+                        <v-card-text>
+                            <v-layout row wrap>
+                                <v-flex xs12 lg6>
+                                    <v-menu ref="menu1" v-model="menu1"
+                                        :close-on-content-click="false" :nudge-right="40"
+                                        lazy transition="scale-transition"
+                                        offset-y full-width max-width="290px" min-width="290px" >
+                                        <template v-slot:activator="{ on }">
+                                            <v-text-field
+                                                v-model="startDate"
+                                                label="开始日期"
+                                                persistent-hint
+                                                readonly
+                                                prepend-icon="event"
+                                                @blur="startDate = parseDate(dateFormatted)"
+                                                v-on="on" ></v-text-field>
+                                        </template>
+                                            <v-date-picker v-model="startDate" no-title @input="menu1 = false"></v-date-picker>
+                                    </v-menu>
+                                </v-flex>
+                            
+                                <v-flex xs12 lg6>
+                                    <v-menu
+                                        v-model="menu2"
+                                        :close-on-content-click="false"
+                                        :nudge-right="40"
+                                        lazy
+                                        transition="scale-transition"
+                                        offset-y
+                                        full-width
+                                        max-width="290px"
+                                        min-width="290px"
+                                    >
+                                        <template v-slot:activator="{ on }">
+                                        <v-text-field
+                                            v-model="endDate"
+                                            label="结束日期"
+                                            persistent-hint
+                                            prepend-icon="event"
+                                            readonly
+                                            v-on="on"
+                                        ></v-text-field>
+                                        </template>
+                                        <v-date-picker v-model="endDate" no-title @input="menu2 = false"></v-date-picker>
+                                    </v-menu>
+                                </v-flex>
+                            </v-layout>
+                        </v-card-text>
+                    </v-card>
+                </v-dialog>
+            </v-card-actions>
         </v-card>
     </v-flex>
     <v-flex sm12 md6>
@@ -155,6 +215,34 @@ export default {
                 series: [
                     {type: 'bar'}
                 ]
+            },
+            startDate: '', endDate: '',
+            datePickerDialog: false
+        }
+    },
+    watch: {
+        datePickerDialog(newVal) {
+            if(newVal === false) {
+                // closed
+                this.$axios.get("/stats/books", {
+                    params: {
+                        startTime: this.startDate,
+                        endTime: this.endDate
+                    }
+                })
+                .then(response => {
+                    let ret = response.data
+                    let arr = []
+                    for(var prop in ret) {
+                        arr.push([prop, ret[prop]])
+                    }
+                    this.bookStat.dataset.source = 
+                        [['book', 'amount']].concat(arr)
+                    console.log(this.bookStat.dataset.source)
+                })
+                .catch(error => {
+                    this.$store.commit("setPrompt", "请求失败")
+                })
             }
         }
     },
@@ -167,11 +255,19 @@ export default {
                 arr.push([prop, ret[prop]])
             }
             this.bookStat.dataset.source = 
-                this.bookStat.dataset.source[0].concat(arr)
+                [['book', 'amount']].concat(arr)
         })
-        .error(error => {
+        .catch(error => {
             this.$store.commit("setPrompt", "请求失败")
         })
+    },
+    methods: {
+        parseDate (date) {
+            if (!date)
+                return null
+            const [month, day, year] = date.split('/')
+            return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+        }
     }
 }
 </script>
